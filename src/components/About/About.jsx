acc78c0b5ecedc3aea542dda85fb4493bea8dbf4
unknown <camizupa.absti@gmail.com>
2023-08-31 22:10:29 -0300

    portafolio 2023 (arquitecture, assets and static data)

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,14 +9,14 @@ export default function About({ data, backgroundColorSecondary, backgroundColorP
             <div className="bg-img-coffee">
                 <strong style={{ color: textOposite, fontSize: '86px', paddingLeft: '20px', fontWeight: 'bolder' }}>COFFEE & CODE LOVER</strong>
             </div>
-            <Technologies backgroundColorSecondary={backgroundColorSecondary} backgroundColorPrimary={backgroundColorPrimary} textColor={textColor} fontColor={fontColor} textOposite={textOposite} data={data} />
+            {data&&<Technologies backgroundColorSecondary={backgroundColorSecondary} backgroundColorPrimary={backgroundColorPrimary} textColor={textColor} fontColor={fontColor} textOposite={textOposite} data={data} />}
             {/* <div className="bg-img-pet">
                 <strong style={{ color: textColor, fontSize: '72px', paddingRight: '20px', fontWeight: 'bolder' }}>PET FRIENDLY</strong>
             </div> */}
             <div className="bg-img-team">
                 <strong style={{ color: textOposite, fontSize: '86px', paddingLeft: '20px', fontWeight: 'bolder', cursor: 'pointer' }}>TEAM PLAYER</strong>
             </div>
-            <div style={{ backgroundColor: colors.text.dark, display: "flex", flexWrap: 'wrap', width: '100vw', justifyContent: 'center', alignItems: 'center', padding: '5vh 10vw', position: 'relative' }}>
+            {data&&<div style={{ backgroundColor: colors.text.dark, display: "flex", flexWrap: 'wrap', width: '100vw', justifyContent: 'center', alignItems: 'center', padding: '5vh 10vw', position: 'relative' }}>
                 {data.recommendations.map((e, i) =>
                     <figure className="snip1157" key={i}>
                         <blockquote>{e.content}
@@ -28,7 +28,7 @@ export default function About({ data, backgroundColorSecondary, backgroundColorP
                         </div>
                     </figure>
                 )}
-            </div>
+            </div>}
         </>
     )
 }
